16a181b2af9a44fe6ef95bb48c41489425222bcd
jlgallego99 <jlgallego99@correo.ugr.es>
2023-05-04 21:18:44 +0200

    Cabeceras de persistencia de ficheros de datos #52
    Interfaz comun para escribir en los distintos ficheros de datos

--- tropestogo/media/repository.go
+++ tropestogo/media/repository.go
@@ -1,17 +1,14 @@
 package media
 
-import (
-	"github.com/google/uuid"
-)
-
 // RepositoryMedia defines an interface for all kinds of repositories of media tropes in TvTropes
 // The interface allows us to implement multiple structs that handle different data formats like CSV or JSON
 // sharing common methods
 type RepositoryMedia interface {
-	GetAll() ([]Media, error)
-	Get(uuid.UUID) (Media, error)
-	Add(Media) error
-	Update(Media) error
+	// AddMedia adds a new Media (Work with its Tropes) to the dataset
+	AddMedia(Media) error
+
+	// UpdateMedia updates a Media (Work with its Tropes) within the dataset
+	UpdateMedia(Media) error
 }
 
 type CSVRepository struct {
