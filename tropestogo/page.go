f2827c481fc41a9870f0c5044907403ed457a23b
jlgallego99 <jlgallego99@correo.ugr.es>
2023-05-04 20:57:15 +0200

    Almacenar el html de una pagina como arbol DOM #51
    Requería de añadir la dependencia de Goquery para definir el tipo

--- tropestogo/page.go
+++ tropestogo/page.go
@@ -1,15 +1,20 @@
 package tropestogo
 
 import (
-	"github.com/google/uuid"
+	"github.com/PuerkitoBio/goquery"
 	"net/url"
 	"time"
 )
 
-// Page is a TvTropes raw page for later scraping
+// Page is a TvTropes Work page for later scraping
 type Page struct {
-	ID          uuid.UUID
-	URL         url.URL
-	RawHTML     string
+	// URL defines the identity of the Page entity
+	URL url.URL
+
+	// DOMTree holds a traversable tree of HTML elements comprised of only the main body of a Work page
+	// This has all the information the Scraper needs
+	DOMTree goquery.Document
+
+	// LastUpdated is the last time the page was updated, for helping with maintaining information updated
 	LastUpdated time.Time
 }
