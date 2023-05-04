f306424d5e08cb6f8a9d1ffd1e640c11076db87d
jlgallego99 <jlgallego99@correo.ugr.es>
2023-05-04 21:23:36 +0200

    Cabeceras para recuperar los datos de TvTropes #52

--- tropestogo/media/repository.go
+++ tropestogo/media/repository.go
@@ -1,5 +1,7 @@
 package media
 
+import tropestogo "github.com/jlgallego99/TropesToGo"
+
 // RepositoryMedia defines an interface for all kinds of repositories of media tropes in TvTropes
 // The interface allows us to implement multiple structs that handle different data formats like CSV or JSON
 // sharing common methods
@@ -9,6 +11,15 @@ type RepositoryMedia interface {
 
 	// UpdateMedia updates a Media (Work with its Tropes) within the dataset
 	UpdateMedia(Media) error
+
+	// GetTvTropes returns all Media found in TvTropes
+	GetTvTropes() ([]Media, error)
+
+	// GetMedia returns a Work with its Tropes
+	GetMedia(tropestogo.Work) ([]Media, error)
+
+	// GetMedia returns all Media within a MediaType (for example, all films)
+	GetMediaType(MediaType) ([]Media, error)
 }
 
 type CSVRepository struct {
