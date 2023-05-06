2c724c538aef5d6427a92ee5445e2b671cf4ca0d
jlgallego99 <jlgallego99@correo.ugr.es>
2023-05-06 19:28:03 +0200

    Funciones basicas para configurar la factoria scraper #54

--- tropestogo/service/scraper/scraper.go
+++ tropestogo/service/scraper/scraper.go
@@ -13,6 +13,10 @@ var (
 	ErrNotWorkPage  = errors.New("the page isn't a TvTropes Work page")
 )
 
+// ScraperConfig is an alias for a function that will accept a pointer to a ServiceScraper and modify its fields
+// Each function acts as one configuration for the scraper
+type ScraperConfig func(ss *ServiceScraper) error
+
 type ServiceScraper struct {
 	// TvTropes index
 	index index.RepositoryIndex
@@ -20,8 +24,33 @@ type ServiceScraper struct {
 	data media.RepositoryMedia
 }
 
-func NewServiceScraper() (*ServiceScraper, error) {
-	return &ServiceScraper{}, nil
+// NewServiceScraper takes a variable amount of configuration functions and returns a ServiceScraper with all configs passed
+func NewServiceScraper(cfgs ...ScraperConfig) (*ServiceScraper, error) {
+	ss := &ServiceScraper{}
+	// Apply all config functions
+	for _, cfg := range cfgs {
+		// Configure the service we are creating
+		err := cfg(ss)
+		if err != nil {
+			return nil, err
+		}
+	}
+
+	return ss, nil
+}
+
+func ConfigIndexRepository(ir index.RepositoryIndex) ScraperConfig {
+	return func(ss *ServiceScraper) error {
+		ss.index = ir
+		return nil
+	}
+}
+
+func ConfigRepository(mr media.RepositoryMedia) ScraperConfig {
+	return func(ss *ServiceScraper) error {
+		ss.data = mr
+		return nil
+	}
 }
 
 // CheckValidWorkPage checks if a TvTropes Work page has a valid structure in which the scraper can extract data
