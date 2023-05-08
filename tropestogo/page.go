5a1f51b44881cab1f6d6885dbc00da13d4cd5411
jlgallego99 <jlgallego99@correo.ugr.es>
2023-05-08 17:42:20 +0200

    :recycle: Pagina no almacena documento HTML #51
    Puede acabar siendo muy pesado almacenar todas las paginas, simplemente se extraerá directamente sabiendo la URL

--- tropestogo/page.go
+++ tropestogo/page.go
@@ -1,7 +1,6 @@
 package tropestogo
 
 import (
-	"github.com/PuerkitoBio/goquery"
 	"net/url"
 	"time"
 )
@@ -10,10 +9,6 @@ import (
 type Page struct {
 	// URL defines the identity of the Page entity
 	URL url.URL
-
-	// DOMTree holds a traversable tree of HTML elements comprised of only the main body of a Work page
-	// This has all the information the Scraper needs
-	DOMTree goquery.Document
 
 	// LastUpdated is the last time the page was updated, for helping with maintaining information updated
 	LastUpdated time.Time
