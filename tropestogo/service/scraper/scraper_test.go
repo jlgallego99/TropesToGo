ce23627406df94979d1859ea59a67759d348be36
jlgallego99 <jlgallego99@correo.ugr.es>
2023-05-23 17:32:28 +0200

    :white_check_mark: Reorganizacion de tests #10
    Solo ejecuta las funciones y variables concretas del test, el resto solo una vez antes de la suite
    
    Ahorra tiempo y disminuye peticiones a tvtropes

--- tropestogo/service/scraper/scraper_test.go
+++ tropestogo/service/scraper/scraper_test.go
@@ -12,52 +12,52 @@ import (
 	. "github.com/onsi/gomega"
 )
 
+// A scraper service for test purposes
+var serviceScraper *scraper.ServiceScraper
+var newScraperErr, invalidScraperErr error
+
+// A valid TvTropes page and one page that is from other website
+var tvTropesPage, tvTropesPage2, tvTropesPage3, notTvTropesPage, notWorkPage *tropestogo.Page
+
+var _ = BeforeSuite(func() {
+	serviceScraper, newScraperErr = scraper.NewServiceScraper()
+	// Create invalid scraper
+	_, invalidScraperErr = scraper.NewServiceScraper(scraper.ConfigIndexRepository(nil))
+
+	tvTropesUrl, _ := url.Parse("https://tvtropes.org/pmwiki/pmwiki.php/Film/Oldboy2003")
+	tvTropesUrl2, _ := url.Parse("https://tvtropes.org/pmwiki/pmwiki.php/Film/TheAvengers2012")
+	tvTropesUrl3, _ := url.Parse("https://tvtropes.org/pmwiki/pmwiki.php/Film/ANewHope")
+
+	differentUrl, _ := url.Parse("https://www.google.com/")
+	notWorkUrl, _ := url.Parse("https://tvtropes.org/pmwiki/pmwiki.php/Main/Media")
+
+	tvTropesPage = &tropestogo.Page{
+		URL:         tvTropesUrl,
+		LastUpdated: time.Now(),
+	}
+
+	tvTropesPage2 = &tropestogo.Page{
+		URL:         tvTropesUrl2,
+		LastUpdated: time.Now(),
+	}
+
+	tvTropesPage3 = &tropestogo.Page{
+		URL:         tvTropesUrl3,
+		LastUpdated: time.Now(),
+	}
+
+	notTvTropesPage = &tropestogo.Page{
+		URL:         differentUrl,
+		LastUpdated: time.Now(),
+	}
+
+	notWorkPage = &tropestogo.Page{
+		URL:         notWorkUrl,
+		LastUpdated: time.Now(),
+	}
+})
+
 var _ = Describe("Scraper", func() {
-	// A scraper service for test purposes
-	var serviceScraper *scraper.ServiceScraper
-	var newScraperErr, invalidScraperErr error
-	// A valid TvTropes page and one page that is from other website
-	var tvTropesPage, tvTropesPage2, tvTropesPage3, notTvTropesPage, notWorkPage *tropestogo.Page
-
-	BeforeEach(func() {
-		serviceScraper, newScraperErr = scraper.NewServiceScraper()
-
-		// Create invalid scraper
-		_, invalidScraperErr = scraper.NewServiceScraper(scraper.ConfigIndexRepository(nil))
-
-		tvTropesUrl, _ := url.Parse("https://tvtropes.org/pmwiki/pmwiki.php/Film/Oldboy2003")
-		tvTropesUrl2, _ := url.Parse("https://tvtropes.org/pmwiki/pmwiki.php/Film/TheAvengers2012")
-		tvTropesUrl3, _ := url.Parse("https://tvtropes.org/pmwiki/pmwiki.php/Film/ANewHope")
-
-		differentUrl, _ := url.Parse("https://www.google.com/")
-		notWorkUrl, _ := url.Parse("https://tvtropes.org/pmwiki/pmwiki.php/Main/Media")
-
-		tvTropesPage = &tropestogo.Page{
-			URL:         tvTropesUrl,
-			LastUpdated: time.Now(),
-		}
-
-		tvTropesPage2 = &tropestogo.Page{
-			URL:         tvTropesUrl2,
-			LastUpdated: time.Now(),
-		}
-
-		tvTropesPage3 = &tropestogo.Page{
-			URL:         tvTropesUrl3,
-			LastUpdated: time.Now(),
-		}
-
-		notTvTropesPage = &tropestogo.Page{
-			URL:         differentUrl,
-			LastUpdated: time.Now(),
-		}
-
-		notWorkPage = &tropestogo.Page{
-			URL:         notWorkUrl,
-			LastUpdated: time.Now(),
-		}
-	})
-
 	Describe("Create the scraper service", func() {
 		Context("The service is created correctly", func() {
 			It("Shouldn't return an error", func() {
@@ -80,15 +80,11 @@ var _ = Describe("Scraper", func() {
 		var validTvTropesPage, validTvTropesPage2, validTvTropesPage3, validDifferentPage, validNotWorkPage bool
 		var errTvTropes, errTvTropes2, errTvTropes3, errDifferent, errNotWorkPage error
 
-		BeforeEach(func() {
-			validTvTropesPage, errTvTropes = serviceScraper.CheckValidWorkPage(tvTropesPage)
-			validTvTropesPage2, errTvTropes2 = serviceScraper.CheckValidWorkPage(tvTropesPage2)
-			validTvTropesPage3, errTvTropes3 = serviceScraper.CheckValidWorkPage(tvTropesPage3)
-			validDifferentPage, errDifferent = serviceScraper.CheckValidWorkPage(notTvTropesPage)
-			validNotWorkPage, errNotWorkPage = serviceScraper.CheckValidWorkPage(notWorkPage)
-		})
-
 		Context("URL belongs to a TvTropes Work page with tropes on a list", func() {
+			BeforeEach(func() {
+				validTvTropesPage, errTvTropes = serviceScraper.CheckValidWorkPage(tvTropesPage)
+			})
+
 			It("Should mark the page as valid", func() {
 				Expect(validTvTropesPage).To(BeTrue())
 			})
@@ -99,6 +95,10 @@ var _ = Describe("Scraper", func() {
 		})
 
 		Context("URL belongs to a TvTropes Work page with tropes on subpages", func() {
+			BeforeEach(func() {
+				validTvTropesPage2, errTvTropes2 = serviceScraper.CheckValidWorkPage(tvTropesPage2)
+			})
+
 			It("Should mark the page as valid", func() {
 				Expect(validTvTropesPage2).To(BeTrue())
 			})
@@ -109,6 +109,10 @@ var _ = Describe("Scraper", func() {
 		})
 
 		Context("URL belongs to a TvTropes Work page with tropes on folders", func() {
+			BeforeEach(func() {
+				validTvTropesPage3, errTvTropes3 = serviceScraper.CheckValidWorkPage(tvTropesPage3)
+			})
+
 			It("Should mark the page as valid", func() {
 				Expect(validTvTropesPage3).To(BeTrue())
 			})
@@ -119,6 +123,10 @@ var _ = Describe("Scraper", func() {
 		})
 
 		Context("URL belongs to TvTropes but isn't from a Work page", func() {
+			BeforeEach(func() {
+				validNotWorkPage, errNotWorkPage = serviceScraper.CheckValidWorkPage(notWorkPage)
+			})
+
 			It("Should mark the page as invalid", func() {
 				Expect(validNotWorkPage).To(BeFalse())
 			})
@@ -129,6 +137,10 @@ var _ = Describe("Scraper", func() {
 		})
 
 		Context("URL isn't from TvTropes", func() {
+			BeforeEach(func() {
+				validDifferentPage, errDifferent = serviceScraper.CheckValidWorkPage(notTvTropesPage)
+			})
+
 			It("Should mark the page as invalid", func() {
 				Expect(validDifferentPage).To(BeFalse())
 			})
