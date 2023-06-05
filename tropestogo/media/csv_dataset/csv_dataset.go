e26b4c65399fcd94f3fa7a97e9dd93cc4bd2fd0c
jlgallego99 <jlgallego99@correo.ugr.es>
2023-06-05 21:09:55 +0200

    Actualizar informacion de una pelicula en el CSV #68
    Lee linea a linea el CSV buscando el titulo y año de la entrada para modificarla, distinguiendo peliculas con mismo nombre por el año

--- tropestogo/media/csv_dataset/csv_dataset.go
+++ tropestogo/media/csv_dataset/csv_dataset.go
@@ -59,16 +59,9 @@ func (repository *CSVRepository) GetReader() (*csv.Reader, error) {
 }
 
 func (repository *CSVRepository) AddMedia(media media.Media) error {
-	var tropes []string
-	for trope := range media.GetWork().Tropes {
-		tropes = append(tropes, trope.GetTitle())
-	}
+	record := CreateMediaRecord(media)
 
 	// Add record to the CSV file
-	// A record consists of the following fields: title,year,lastupdated,url,mediatype,tropes
-	record := []string{media.GetWork().Title, media.GetWork().Year, media.GetWork().LastUpdated.Format(time.DateTime),
-		media.GetPage().URL.String(), media.GetMediaType().String(), strings.Join(tropes, ";")}
-
 	// Mutual exclusion access to the repository
 	repository.Lock()
 	err := repository.writer.Write(record)
@@ -78,10 +71,45 @@ func (repository *CSVRepository) AddMedia(media media.Media) error {
 	return err
 }
 
-// UpdateMedia updates a record in the CSV files by searching the title
-func (repository *CSVRepository) UpdateMedia(media media.Media) error {
-	//TODO implement me
-	panic("implement me")
+// UpdateMedia updates a record in the CSV files by searching the title and year
+func (repository *CSVRepository) UpdateMedia(title string, year string, media media.Media) error {
+	reader, errReader := repository.GetReader()
+	if errReader != nil {
+		return errReader
+	}
+
+	records, errReadCSV := reader.ReadAll()
+	if errReadCSV != nil {
+		return errReadCSV
+	}
+
+	// Look for the line that holds the record that needs to be updated
+	updateLine := -1
+	for pos, record := range records {
+		if record[0] == title && record[1] == year {
+			updateLine = pos
+			break
+		}
+	}
+
+	// Update record
+	input, _ := os.ReadFile(repository.name)
+	lines := strings.Split(string(input), "\n")
+	for linePos, _ := range lines {
+		if linePos == updateLine {
+			updatedRecord := CreateMediaRecord(media)
+			lines[linePos] = strings.Join(updatedRecord, ",")
+
+			break
+		}
+	}
+	output := strings.Join(lines, "\n")
+	errWrite := os.WriteFile("dataset.csv", []byte(output), 0644)
+	if errWrite != nil {
+		return errWrite
+	}
+
+	return nil
 }
 
 func (repository *CSVRepository) GetMedia(work tropestogo.Work) ([]media.Media, error) {
@@ -110,4 +138,18 @@ func (repository *CSVRepository) RemoveAll() error {
 	} else {
 		return ErrFileNotExists
 	}
+}
+
+// CreateMediaRecord forms a string properly separated for inserting in a CSV file
+func CreateMediaRecord(media media.Media) []string {
+	var tropes []string
+	for trope := range media.GetWork().Tropes {
+		tropes = append(tropes, trope.GetTitle())
+	}
+
+	// A record consists of the following fields: title,year,lastupdated,url,mediatype,tropes
+	record := []string{media.GetWork().Title, media.GetWork().Year, media.GetWork().LastUpdated.Format(time.DateTime),
+		media.GetPage().URL.String(), media.GetMediaType().String(), strings.Join(tropes, ";")}
+
+	return record
 }
