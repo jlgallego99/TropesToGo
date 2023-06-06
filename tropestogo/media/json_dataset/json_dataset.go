e934b5708aaae281737a3ba0d3e6f52a5a107b8c
jlgallego99 <jlgallego99@correo.ugr.es>
2023-06-06 16:26:49 +0200

    Crea JSON implementando la interfaz repositorio de Media #68

--- tropestogo/media/json_dataset/json_dataset.go
+++ tropestogo/media/json_dataset/json_dataset.go
@@ -1,7 +1,37 @@
 package json_dataset
 
-import "sync"
+import (
+	"github.com/jlgallego99/TropesToGo/media"
+	"os"
+	"sync"
+)
 
 type JSONRepository struct {
 	sync.Mutex
+	name string
+}
+
+func NewJSONRepository(name string) (*JSONRepository, error) {
+	_, err := os.Create(name + ".json")
+
+	repository := &JSONRepository{
+		name: name + ".json",
+	}
+
+	return repository, err
+}
+
+func (repository *JSONRepository) AddMedia(media media.Media) error {
+	//TODO implement me
+	panic("implement me")
+}
+
+func (repository *JSONRepository) UpdateMedia(s string, s2 string, media media.Media) error {
+	//TODO implement me
+	panic("implement me")
+}
+
+func (repository *JSONRepository) RemoveAll() error {
+	//TODO implement me
+	panic("implement me")
 }
---
+++ tropestogo/media/json_dataset/json_dataset_test.go
@@ -0,0 +1,12 @@
+package json_dataset_test
+
+import (
+	. "github.com/onsi/ginkgo/v2"
+	. "github.com/onsi/gomega"
+
+	"github.com/jlgallego99/TropesToGo/media/json_dataset"
+)
+
+var _ = Describe("JsonDataset", func() {
+
+})
