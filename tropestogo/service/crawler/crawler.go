ad62209e3094678eca8eb5ca599b629a64d5b98f
jlgallego99 <jlgallego99@correo.ugr.es>
2023-06-22 02:08:36 +0200

    Punto de partida del crawler: indice de Works #55
    Con esta pagina de TvTropes se pueden encontrar todos los trabajos de cualquier Media, indicandolo como parametro en la URL
    
    Se crea como un objeto Page de categoria Indice

--- tropestogo/service/crawler/crawler.go
+++ tropestogo/service/crawler/crawler.go
@@ -1,12 +1,45 @@
 package crawler
 
-import "net/url"
+import (
+	"errors"
+	"fmt"
+	tropestogo "github.com/jlgallego99/TropesToGo"
+	"github.com/jlgallego99/TropesToGo/media"
+	"net/url"
+)
+
+const Pagelist = "https://tvtropes.org/pmwiki/pagelist_having_pagetype_in_namespace.php?t=work"
+
+var (
+	ErrNotFound = errors.New("couldn't request the URL")
+	ErrBadUrl   = errors.New("invalid URL")
+)
 
 type ServiceCrawler struct {
-	// Seeds are the base URLs of the crawler
-	seeds []url.URL
+	// The seed is the starting URL of the crawler
+	seed tropestogo.Page
 }
 
 func NewCrawler() (*ServiceCrawler, error) {
 	return nil, nil
+}
+
+func (crawler *ServiceCrawler) SetMediaSeed(mediaType media.MediaType) error {
+	seedUrl, errParse := url.Parse(Pagelist)
+	if errParse != nil {
+		return fmt.Errorf("%w: "+Pagelist+"\n%w", ErrBadUrl, errParse)
+	}
+
+	values := seedUrl.Query()
+	values.Add("n", mediaType.String())
+	seedUrl.RawQuery = values.Encode()
+
+	seedPage, errNewPage := tropestogo.NewPage(seedUrl.String())
+	if errNewPage != nil {
+		return errNewPage
+	}
+
+	crawler.seed = seedPage
+
+	return nil
 }
